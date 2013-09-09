f37f2687df32391de6d4deb69077eae378e4349b
rene588 <e.aquino.taipe@isur.edu.pe>
2013-09-09 17:51:54 -0500

    nuevooo

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -22,28 +22,40 @@ namespace Calculadora
         }
 
         private void button1_Click(object sender, EventArgs e) 
-        
 
         {
-            float cent = float.Parse(this.gradoscent.Text );
-            float fahr = (cent * 9 / 5) + 32;
-            this.resultado.Text = fahr.ToString();
-            float  kel = (cent + 273);
-            this.resultado2.Text = kel.ToString();
+            if ( gradoscent.Text == "" || num2.Text == "" || num3.Text =="" )
+            {
+                MessageBox.Show("llenar los tres espacios");
+            
+            }
+            else 
+            {
+                float cent = float.Parse(this.gradoscent.Text);
+                float fahr = (cent * 9 / 5) + 32;
+                this.resultado.Text = fahr.ToString();
+                float kel = (cent + 273);
+                this.resultado2.Text = kel.ToString();
 
-            float fah = float.Parse(this.num2.Text);
-            float cen = (5*(fah - 32)) / 9;
-            this.resu1.Text = cen.ToString();
-            float kelv = ((5 * (fah - 32)) / 9) + 273;
-            this.resu2.Text = kelv.ToString();
+                float fah = float.Parse(this.num2.Text);
+                float cen = (5 * (fah - 32)) / 9;
+                this.resu1.Text = cen.ToString();
+                float kelv = ((5 * (fah - 32)) / 9) + 273;
+                this.resu2.Text = kelv.ToString();
 
-            float kelvi = float.Parse(this.num3.Text);
-            float centi = kelvi - 273;
-            this.res1.Text = centi.ToString();
-            float fa = ((9 / (kelvi - 273)) / 5) + 32;
-            this.res2.Text = fa.ToString();
-
+                float kelvi = float.Parse(this.num3.Text);
+                float centi = kelvi - 273;
+                this.res1.Text = centi.ToString();
+                float fa = ((9 / (kelvi - 273)) / 5) + 32;
+                this.res2.Text = fa.ToString();
+             
+            }
  
+        }
+
+        private void gradoscent_TextChanged(object sender, EventArgs e)
+        {
+
         }
         
 
